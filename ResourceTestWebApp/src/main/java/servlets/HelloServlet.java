45426e289e3dda8fe3ae32846d476e09756ac94d
Jay SenSharma <joy.sensharma@gmail.com>
2015-07-14 17:12:10 +0530

    JBoss AS7 plugin based simle servlet demo

---
+++ ResourceTestWebApp/src/main/java/servlets/HelloServlet.java
@@ -0,0 +1,19 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.Writer;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+   @Override
+   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+      Writer writer = resp.getWriter();
+      writer.write("<html><body>");
+      writer.write("<h1>Hello World !!!</h1> (from HelloServlet)");
+      writer.write("</body></html>");
+   }
+}
---
+++ ResourceTestWebApp/src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<html>
+  <head>
+     <title>Simple Page redirect in 2 seconds</title>
+     <meta http-equiv="refresh" content="2; URL=HelloServlet">
+  </head>
+  <body>
+     <h1> this page will be automatically redirected to HelloServlet</h1>
+  </body>
+</html>
